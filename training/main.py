d6446a09f09f70a230b6f2346c54d1468f1bc972
Milton Chandradas <milton.chandradas@sap.com>
2024-02-21 02:52:28 -0500

    training...

---
+++ training/main.py
@@ -0,0 +1,37 @@
+import os
+import pandas as pd
+
+# Variables
+DATA_PATH = '/app/data/train.csv'
+CLASS_PATH= int(os.getenv('CLASS_PATH'))
+KERNEL= int(os.getenv('KERNEL'))
+MODEL_PATH = '/app/model/model.pkl'
+
+# Load Datasets
+import pandas as pd
+df = pd.read_csv(DATA_PATH)
+# df = pd.read_csv('irisdataset.csv')
+X = df.drop('target', axis=1)
+y = df['target']
+
+# Partition into Train and test dataset
+from sklearn.model_selection import train_test_split
+train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
+
+# Init model
+from sklearn import svm
+model = svm.SVC(C=CLASS_PATH, kernel=KERNEL)
+# model = svm.SVC(C=1, kernel="rbf")
+
+# Train model
+model.fit(train_x, train_y)
+
+# Test model
+score = model.score(test_x, test_y)
+# Output will be available in logs of SAP AI Core.
+# Not the ideal way of storing /reporting metrics in SAP AI Core, but that is not the focus this tutorial
+print(f"Test Data Score: {score}")
+
+# Save model
+import pickle
+pickle.dump(model, open(MODEL_PATH, 'wb'))
